f7ab04e9ebedf008d508cd26b746ce05ce000f9f
F286 <freedebreuil@gmail.com>
2022-04-25 23:51:59 -0500

    split kernel to separate file

--- Samples/0_Introduction/vectorAdd/vectorAdd.cu
+++ Samples/0_Introduction/vectorAdd/vectorAdd.cu
@@ -6,29 +6,6 @@
 #include "ML_Neuron.h"
 #include "vectorMultiply.cuh"
 
-__global__ void vectorMultiply(const ML_DeviceMatrix<float> input, const ML_DeviceMatrix<float> connection, ML_DeviceMatrix<float> output) {
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-
-    if (i < output.Count())
-    {
-        const float* connectionStart = &connection[i * input.dimensions.x];
-
-        float total = 0.0f;
-        for (int elementIndex = 0; elementIndex < input.dimensions.x; elementIndex++)
-        {
-            total += input[elementIndex] * connectionStart[elementIndex];
-        }
-        output[i] = total;
-    }
-}
-void Multiply(ML_Matrix<float>& input, ML_Matrix<float>& connection, ML_Matrix<float>& output)
-{
-    ML_Helpers::VerifyForwardConnection(input.Dimensions(), connection.Dimensions(), output.Dimensions());
-    ML_CheckCudaError checkError;
-    ML_KernelSize size{ output.Dimensions() };
-    vectorMultiply CUDA_KERNEL(size.blocksPerGrid, size.threadsPerBlock)(input.DeviceArray(), connection.DeviceArray(), output.DeviceArray());
-}
-
 __global__ void vectorDivide(const ML_DeviceMatrix<float> input, const ML_DeviceMatrix<float> connection, ML_DeviceMatrix<float> output) {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
 
--- Samples/0_Introduction/vectorAdd/vectorMultiply.cu
+++ Samples/0_Introduction/vectorAdd/vectorMultiply.cu
@@ -1,27 +1,27 @@
-//#pragma once
-//#include "ML_Helpers.h"
-//#include "ML_Array.h"
-//
-//__global__ void vectorMultiply(const ML_DeviceMatrix<float> input, const ML_DeviceMatrix<float> connection, ML_DeviceMatrix<float> output) {
-//    int i = blockDim.x * blockIdx.x + threadIdx.x;
-//
-//    if (i < output.Count())
-//    {
-//        const float* connectionStart = &connection[i * input.dimensions.x];
-//
-//        float total = 0.0f;
-//        for (int elementIndex = 0; elementIndex < input.dimensions.x; elementIndex++)
-//        {
-//            total += input[elementIndex] * connectionStart[elementIndex];
-//        }
-//        output[i] = total;
-//    }
-//}
-//void Multiply(ML_Matrix<float>& input, ML_Matrix<float>& connection, ML_Matrix<float>& output)
-//{
-//    ML_Helpers::VerifyForwardConnection(input.Dimensions(), connection.Dimensions(), output.Dimensions());
-//    ML_CheckCudaError checkError;
-//    ML_KernelSize size{ output.Dimensions() };
-//    vectorMultiply CUDA_KERNEL(size.blocksPerGrid, size.threadsPerBlock)(input.DeviceArray(), connection.DeviceArray(), output.DeviceArray());
-//}
-//
+#pragma once
+#include "ML_Helpers.h"
+#include "ML_Array.h"
+
+__global__ void vectorMultiply(const ML_DeviceMatrix<float> input, const ML_DeviceMatrix<float> connection, ML_DeviceMatrix<float> output) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (i < output.Count())
+    {
+        const float* connectionStart = &connection[i * input.dimensions.x];
+
+        float total = 0.0f;
+        for (int elementIndex = 0; elementIndex < input.dimensions.x; elementIndex++)
+        {
+            total += input[elementIndex] * connectionStart[elementIndex];
+        }
+        output[i] = total;
+    }
+}
+void Multiply(ML_Matrix<float>& input, ML_Matrix<float>& connection, ML_Matrix<float>& output)
+{
+    ML_Helpers::VerifyForwardConnection(input.Dimensions(), connection.Dimensions(), output.Dimensions());
+    ML_CheckCudaError checkError;
+    ML_KernelSize size{ output.Dimensions() };
+    vectorMultiply CUDA_KERNEL(size.blocksPerGrid, size.threadsPerBlock)(input.DeviceArray(), connection.DeviceArray(), output.DeviceArray());
+}
+
